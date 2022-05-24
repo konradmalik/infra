915b4be7d929a4b3df4ab26934ddf5318dd9d601
Konrad Malik <konrad.malik@gmail.com>
2022-05-24 13:27:39 +0200

    daemonize crashes qemu on macos after some time (???)

--- environments/dev/qemu/devarch/headless.sh
+++ environments/dev/qemu/devarch/headless.sh
@@ -6,7 +6,8 @@ qemu-system-x86_64 \
 	-smp 6 \
 	-m 8G \
     -display none \
-    -daemonize \
 	-device virtio-net,netdev=vmnic -netdev user,id=vmnic,hostfwd=tcp::2222-:22 \
 	-drive file="$HOME/VMs/devarch.qcow2",if=virtio \
+    -D devarch.log \
     ;
+    #-daemonize \
