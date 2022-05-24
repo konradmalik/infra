c5f77e0509d09827064d5aa2b13acc8cc9999060
Konrad Malik <konrad.malik@gmail.com>
2022-05-24 21:56:17 +0200

    don't log devarch

--- environments/dev/qemu/devarch/headless.sh
+++ environments/dev/qemu/devarch/headless.sh
@@ -8,6 +8,5 @@ qemu-system-x86_64 \
     -display none \
 	-device virtio-net,netdev=vmnic -netdev user,id=vmnic,hostfwd=tcp::2222-:22 \
 	-drive file="$HOME/VMs/devarch.qcow2",if=virtio \
-    -D devarch.log \
     ;
     #-daemonize \
