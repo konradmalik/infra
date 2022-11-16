cdffc6272f735230a1bf18496e9da3f009098996
Konrad Malik <konrad.malik@gmail.com>
2022-11-16 08:34:27 +0100

    small flake updates

--- environments/dev/nix/flake.nix
+++ environments/dev/nix/flake.nix
@@ -1,7 +1,5 @@
 {
-  description = "A Konrad's home flake";
-  # original idea https://gist.github.com/suhr/4bb1f8434d0622588b23f9fe13e79973
-  # https://gist.github.com/lheckemann/402e61e8e53f136f239ecd8c17ab1deb
+  description = "Konrad's home flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
