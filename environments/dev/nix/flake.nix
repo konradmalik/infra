7c140d108298cd3de22c4c5cecd240a38c000d14
Konrad Malik <konrad.malik@gmail.com>
2022-12-10 20:37:40 +0100

    nix flake decouple into default.nix

--- environments/dev/nix/flake.nix
+++ environments/dev/nix/flake.nix
@@ -2,63 +2,28 @@
   description = "Konrad's home flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = github:NixOS/nixpkgs;
+    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            unstable-overlay
-            unstable-legacy-overlay
-            legacy-overlay
+            konrad-overlay
           ];
         };
 
-        unstable-overlay = final: prev: {
-          unstable = import nixpkgs-unstable {
-            system = prev.system;
-          };
+        konrad-overlay = final: prev: {
+          konrad = final.callPackage ./. { };
         };
-        unstable-legacy-overlay = final: prev: {
-          unstable-legacy = nixpkgs-unstable.legacyPackages.${prev.system};
-        };
-        legacy-overlay = final: prev: {
-          legacy = nixpkgs.legacyPackages.${prev.system};
-        };
-        # can be here as it's not really system dependent
-        # if ever becomes that - we can move it to outputs and refer to it as 'self.profileExports.${system}'
-        profileExports = pkgs.writeText "profile-exports" ''
-          export MANPATH=$HOME/.nix-profile/share/man:/nix/var/nix/profiles/default/share/man:/usr/share/man
-          export INFOPATH=$HOME/.nix-profile/share/info:/nix/var/nix/profiles/default/share/info:/usr/share/info
-        '';
+
       in
       {
-        packages.default = pkgs.buildEnv {
-          name = "konrad-home-env";
-          paths = [
-            (pkgs.runCommand "profile" { } ''
-              mkdir -p $out/etc/profile.d
-              cp ${profileExports} $out/etc/profile.d/profile-exports.sh
-            '')
+        packages.default = pkgs.konrad.home;
 
-            pkgs.unstable.direnv
-            pkgs.unstable.nix-direnv
-          ];
-          pathsToLink = [ "/share" "/bin" "/etc" ];
-          extraOutputsToInstall = [ "man" "doc" "info" ];
-          postBuild = ''
-            if [ -x $out/bin/install-info -a -w $out/share/info ]; then
-              shopt -s nullglob
-              for i in $out/share/info/*.info $out/share/info/*.info.gz; do
-                  $out/bin/install-info $i $out/share/info/dir
-              done
-            fi
-          '';
-        };
+        formatter = pkgs.nixpkgs-fmt;
       });
 }
