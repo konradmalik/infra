bbc345502e1e8cf42862e84a926bdce96b7e795d
Konrad Malik <konrad.malik@gmail.com>
2023-01-28 12:45:03 +0100

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "konradmalik infra";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
