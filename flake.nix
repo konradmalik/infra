e66efc90f018c7d359b7ddd3476d405702191866
Konrad Malik <konrad.malik@gmail.com>
2024-05-23 22:00:26 +0200

    style: new formatter

--- flake.nix
+++ flake.nix
@@ -5,33 +5,39 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system:
-            function (import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            }));
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            function (
+              import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              }
+            )
+          );
     in
     {
       devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell
-          {
-            name = "infra";
-            packages = with pkgs;[
-              awscli2
-              terraform
-            ];
-          };
+        default = pkgs.mkShell {
+          name = "infra";
+          packages = with pkgs; [
+            awscli2
+            terraform
+          ];
+        };
       });
 
-      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
     };
 }
